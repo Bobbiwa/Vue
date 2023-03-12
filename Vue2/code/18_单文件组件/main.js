ea609808af1b37b3429bda7be995db1c8df2c840
Damon <2696774557@qq.com>
2023-03-12 21:30:46 +0800

    inito

---
+++ Vue2/code/18_单文件组件/main.js
@@ -0,0 +1,10 @@
+import App from './App.vue'
+
+new Vue({
+  template:`
+  <App/>
+  `,
+  components:{
+    App
+  }
+}).$mount('root')
---
+++ Vue2/code/18_单文件组件/student.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>学生名称：{{name}}</h1>
+    <h1>学生地址：{{address}}</h1>
+  </div>
+</template>
+
+<script>
+export default Student = Vue.extend({
+  data() {
+    return {
+      name: '大斌',
+      address: '大菊家'
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
